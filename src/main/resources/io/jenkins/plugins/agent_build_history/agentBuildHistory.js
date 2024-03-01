04670e70f65b64a84a988b221ba683406e156c76
Markus Winter <m.winter@sap.com>
2024-03-01 15:53:33 +0100

    improve agent history table
    use the same approach to have only 1 row with hidden divs instead of
    separate rows per node. This allows to sort the table.
    Make the table width auto to avoid big gaps between the columns on large
    screens.

--- src/main/resources/io/jenkins/plugins/agent_build_history/agentBuildHistory.js
+++ src/main/resources/io/jenkins/plugins/agent_build_history/agentBuildHistory.js
@@ -3,39 +3,37 @@ function generateSVGIcon(iconName) {
   return icons.content.querySelector(`#${iconName}`).cloneNode(true);
 }
 
-Behaviour.specify("button.toggle-flow-nodes", "abh-toggle-flow-nodes", 0 , function(button) {
-  button.onclick = function() {
-    let tr = button.closest("tr");
-    let runId = tr.dataset.id;
-    let table = tr.closest("table");
-    let rows = table.querySelectorAll("tr[data-run-id='" + runId + "'");
-    rows.forEach((row) => {
-      row.classList.toggle("jenkins-hidden");
-    });
-    if (button.dataset.hidden === "true") {
-      button.dataset.hidden = "false";
-      button.textContent = table.dataset.hideNodesText;
-    } else {
-      button.dataset.hidden = "true";
-      button.textContent = table.dataset.showNodesText;
-    }
-  }
-});
+function createCellList(td) {
+  let list = document.createElement("div");
+  list.classList.add("abh-cell-list");
+  td.appendChild(list);
+  return list;
+}
 
-Behaviour.specify(".abh-agent-list__button button", "abh-agent-list__button", 0, function(button) {
+function addCellRow(cellList, content, hidden) {
+  const cell = document.createElement("div");
+  cellList.appendChild(cell);
+  cell.textContent = content;
+  if (hidden) {
+    cell.classList.add("abh-hidden", "jenkins-hidden");
+  }
+  return cell;
+}
+
+Behaviour.specify(".abh-list__button button", "abh-list__button", 0, function(button) {
   button.onclick = function() {
     let tr = button.closest("tr");
     let table = button.closest("table");
-    let rows = tr.querySelectorAll(".abh-agent-hidden");
+    let rows = tr.querySelectorAll(".abh-hidden");
     for (row of rows) {
       row.classList.toggle("jenkins-hidden");
     }
     if (button.dataset.hidden === "true") {
       button.dataset.hidden = "false";
-      button.setAttribute("tooltip", table.dataset.hideAgentsText);
+      button.setAttribute("tooltip", table.dataset.hideText);
     } else {
       button.dataset.hidden = "true";
-      button.setAttribute("tooltip", table.dataset.showAgentsText);
+      button.setAttribute("tooltip", table.dataset.showText);
     }
     Behaviour.applySubtree(tr);
   }
@@ -65,7 +63,7 @@ window.abhBuildTimeTrend = function (data) {
     let link2 = document.createElement("a");
     link2.href = rootURL + "/" + e.url;
     link2.classList.add("model-link", "inside");
-    link2.innerText = escapeHTML(e.displayName);
+    link2.innerText = e.displayName;
     td2.appendChild(link2);
     tr.appendChild(td2);
 
@@ -86,15 +84,10 @@ window.abhBuildTimeTrend = function (data) {
     const agentCount = e.agents.length;
     if (agentCount > 0) {
       let i = 0;
-      let tdAgentList = document.createElement("div");
-      let tdLabelList = document.createElement("div");
-      let tdDurationList = document.createElement("div");
-      tdAgentList.classList.add("abh-agent-list");
-      tdLabelList.classList.add("abh-label-list");
-      tdDurationList.classList.add("abh-duration-list");
-      td5.appendChild(tdAgentList);
-      td6.appendChild(tdLabelList);
-      td7.appendChild(tdDurationList);
+
+      let tdAgentList = createCellList(td5);
+      let tdLabelList = createCellList(td6);
+      let tdDurationList = createCellList(td7);
       for (const agent of e.agents) {
         i++;
         let a = document.createElement("div");
@@ -104,9 +97,9 @@ window.abhBuildTimeTrend = function (data) {
         tdLabelList.appendChild(l);
         tdDurationList.appendChild(duration);
         if (i > 1) {
-          a.classList.add("abh-agent-hidden", "jenkins-hidden");
-          l.classList.add("abh-agent-hidden", "jenkins-hidden");
-          duration.classList.add("abh-agent-hidden", "jenkins-hidden");
+          a.classList.add("abh-hidden", "jenkins-hidden");
+          l.classList.add("abh-hidden", "jenkins-hidden");
+          duration.classList.add("abh-hidden", "jenkins-hidden");
         }
         let buildInfo = null;
         let buildInfoStr = escapeHTML(agent.builtOnStr || "");
@@ -133,9 +126,9 @@ window.abhBuildTimeTrend = function (data) {
       }
       if (agentCount > 1) {
         let d = document.createElement("div");
-        d.classList.add("abh-agent-list__button");
+        d.classList.add("abh-list__button");
         let b = document.createElement("button");
-        b.setAttribute("tooltip", table.dataset.showAgentsText);
+        b.setAttribute("tooltip", table.dataset.showText);
         b.appendChild(generateSVGIcon("chevron-down"));
         b.classList.add("jenkins-button");
         b.dataset.hidden = "true";
@@ -172,99 +165,16 @@ window.abhDisplayExtendedBuildHistory = function(data) {
   const table = document.getElementById("projectStatus");
   table.style.display = "";
 
-  function addFlowNodes(data, table) {
-    for (let x = 0; data.flowNodes.length > x; x++) {
-      const e = data.flowNodes[x];
-      const tr = document.createElement("tr");
-
-      tr.dataset.runId = data.runId;
-      tr.classList.add("jenkins-hidden");
-
-      let td1 = document.createElement("td");
-      td1.setAttribute("data", data.iconColorOrdinal);
-      td1.classList.add("jenkins-table__cell--tight", "jenkins-table__icon");
-      let div1 = document.createElement("div");
-      div1.classList.add("jenkins-table__cell__button-wrapper");
-      let svg = generateSVGIcon(data.iconName);
-      div1.appendChild(svg);
-      td1.appendChild(div1);
-      tr.appendChild(td1);
-
-
-      let td2 = document.createElement("td");
-      let a1 = document.createElement("a");
-      a1.classList.add("jenkins-table__link", "model-link");
-      a1.href = rootUrl + "/" + data.parentUrl;
-      let span1 = document.createElement("span");
-      // TODO port Functions#breakableString to JavaScript and use .textContent rather than .innerHTML
-      span1.innerHTML = data.parentFullDisplayName;
-      a1.appendChild(span1);
-      td2.appendChild(a1);
-      let a2 = document.createElement("a");
-      a2.classList.add(
-        "jenkins-table__link",
-        "jenkins-table__badge",
-        "model-link",
-        "inside",
-      );
-      a2.href = rootUrl + "/" + data.url;
-      a2.textContent = data.displayName;
-      td2.appendChild(a2);
-      tr.appendChild(td2);
-
-      let span2 = document.createElement("span");
-      span2.classList.add("jenkins-!-margin-left-1");
-      span2.textContent = "Node: " + e.flowNodeId;
-      td2.appendChild(span2);
-      tr.appendChild(td2);
-
-      let td3a = document.createElement("td");
-      tr.appendChild(td3a);
-
-      let td3 = document.createElement("td");
-      td3.setAttribute("data", e.startTime);
-      td3.textContent = e.startTimeString;
-      tr.appendChild(td3);
-
-      let td4 = document.createElement("td");
-      td4.setAttribute("data", e.duration);
-      td4.textContent = e.durationString;
-      tr.appendChild(td4);
-
-      let td5 = document.createElement("td");
-      td5.textContent = e.flowNodeStatusMessage;
-      if (e.flowNodeStatusWorse) {
-        td5.style.color = "var(--red)";
-      }
-      tr.appendChild(td5);
-
-
-      let td6 = document.createElement("td");
-      td6.classList.add("jenkins-table__cell--tight");
-      let div2 = document.createElement("div");
-      div2.classList.add("jenkins-table__cell__button-wrapper");
-      let a3 = document.createElement("a");
-      a3.classList.add("jenkins-table__button");
-      a3.href = data.consoleUrl;
-      a3.appendChild(generateSVGIcon("console"));
-      div2.appendChild(a3);
-      td6.appendChild(div2);
-      tr.appendChild(td6);
-      table.tBodies[0].appendChild(tr);
-      Behaviour.applySubtree(tr);
-    }
-  }
-
   for (let x = 0; data.length > x; x++) {
-    const e = data[x];
+    const run = data[x];
     const tr = document.createElement("tr");
 
     let td1 = document.createElement("td");
-    td1.setAttribute("data", e.iconColorOrdinal);
+    td1.setAttribute("data", run.iconColorOrdinal);
     td1.classList.add("jenkins-table__cell--tight", "jenkins-table__icon");
     let div1 = document.createElement("div");
     div1.classList.add("jenkins-table__cell__button-wrapper");
-    let svg = generateSVGIcon(e.iconName);
+    let svg = generateSVGIcon(run.iconName);
     div1.appendChild(svg);
     td1.appendChild(div1);
     tr.appendChild(td1);
@@ -273,10 +183,10 @@ window.abhDisplayExtendedBuildHistory = function(data) {
     td2.classList.add("no-wrap");
     let a1 = document.createElement("a");
     a1.classList.add("jenkins-table__link", "model-link");
-    a1.href = rootUrl + "/" + e.parentUrl;
+    a1.href = rootUrl + "/" + run.parentUrl;
     let span1 = document.createElement("span");
     // TODO port Functions#breakableString to JavaScript and use .textContent rather than .innerHTML
-    span1.innerHTML = e.parentFullDisplayName;
+    span1.innerHTML = run.parentFullDisplayName;
     a1.appendChild(span1);
     td2.appendChild(a1);
     let a2 = document.createElement("a");
@@ -286,52 +196,72 @@ window.abhDisplayExtendedBuildHistory = function(data) {
       "model-link",
       "inside",
     );
-    a2.href = rootUrl + "/" + e.url;
-    a2.textContent = e.displayName;
+    a2.href = rootUrl + "/" + run.url;
+    a2.textContent = run.displayName;
     td2.appendChild(a2);
     tr.appendChild(td2);
-    let td3a = document.createElement("td");
-    if (e.runId !== "") {
-      tr.dataset.id = e.runId;
+
+    let tdTimeSince = document.createElement("td");
+    let tdDuration = document.createElement("td");
+    let tdStatus = document.createElement("td");
+    let tdButton = document.createElement("td");
+
+    let tdTimeSinceList = createCellList(tdTimeSince);
+    let tdDurationList = createCellList(tdDuration);
+    let tdStatusList = createCellList(tdStatus);
+    let tdNodeList = createCellList(tdButton);
+
+    tdTimeSince.setAttribute("data", run.timestampString2);
+    addCellRow(tdTimeSinceList, run.timestampString, false);
+
+    tdDuration.setAttribute("data", run.duration);
+    addCellRow(tdDurationList, escapeHTML(run.durationString), false);
+
+    if (run.buildStatusSummaryWorse) {
+      tdStatus.style.color = "var(--red)";
+    }
+    addCellRow(tdStatusList, run.buildStatusSummaryMessage, false);
+
+    if (run.runId !== "") {
+      tr.dataset.id = run.runId;
+      let d = document.createElement("div");
+      d.classList.add("abh-list__button");
       let button = document.createElement("button");
-      button.classList.add("jenkins-table__button", "jenkins-table__badge", "toggle-flow-nodes");
-      button.textContent = table.dataset.showNodesText;
+      button.setAttribute("tooltip", table.dataset.showText);
+      button.classList.add("jenkins-button", "toggle-flow-nodes");
+      button.appendChild(generateSVGIcon("chevron-down"));
       button.dataset.hidden = "true";
-      td3a.appendChild(button);
+      d.appendChild(button);
+      tdNodeList.appendChild(d);
+      for (let x = 0; run.flowNodes.length > x; x++) {
+        const e = run.flowNodes[x];
+        addCellRow(tdTimeSinceList, e.startTimeString, true);
+        addCellRow(tdDurationList, e.durationString, true);
+        addCellRow(tdNodeList,"Node: " + e.flowNodeId, true);
+        const status = addCellRow(tdStatusList, e.flowNodeStatusMessage, true);
+        if (e.flowNodeStatusWorse) {
+          status.style.color = "var(--red)";
+        }
+      }
     }
-    tr.appendChild(td3a);
 
-    let td3 = document.createElement("td");
-    td3.setAttribute("data", e.timestampString2);
-    td3.textContent = e.timestampString;
-    tr.appendChild(td3);
-
-    let td4 = document.createElement("td");
-    td4.setAttribute("data", e.duration);
-    td4.innerText = escapeHTML(e.durationString);
-    tr.appendChild(td4);
-
-    let td5 = document.createElement("td");
-    if (e.buildStatusSummaryWorse) {
-      td5.style.color = "var(--red)";
-    }
-    td5.textContent = e.buildStatusSummaryMessage;
-    tr.appendChild(td5);
-
-    let td6 = document.createElement("td");
-    td6.classList.add("jenkins-table__cell--tight");
+    let tdConsole = document.createElement("td");
+    tdConsole.classList.add("jenkins-table__cell--tight");
     let div2 = document.createElement("div");
     div2.classList.add("jenkins-table__cell__button-wrapper");
     let a3 = document.createElement("a");
     a3.classList.add("jenkins-table__button");
-    a3.href = e.consoleUrl;
+    a3.href = run.consoleUrl;
     a3.appendChild(generateSVGIcon("console"));
     div2.appendChild(a3);
-    td6.appendChild(div2);
-    tr.appendChild(td6);
-    table.tBodies[0].appendChild(tr);
+    tdConsole.appendChild(div2);
 
-    addFlowNodes(e, table);
+    tr.appendChild(tdTimeSince);
+    tr.appendChild(tdDuration);
+    tr.appendChild(tdStatus);
+    tr.appendChild(tdButton);
+    tr.appendChild(tdConsole);
+    table.tBodies[0].appendChild(tr);
 
     Behaviour.applySubtree(tr);
   }
