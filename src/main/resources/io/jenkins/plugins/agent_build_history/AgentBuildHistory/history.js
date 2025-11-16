550e15f0bcb3a394d2d8aba9e7321f85a04acb26
Markus Winter <m.winter@sap.com>
2025-11-16 02:04:34 +0100

    cleanup and some minor ui tweaks

--- src/main/resources/io/jenkins/plugins/agent_build_history/AgentBuildHistory/history.js
+++ src/main/resources/io/jenkins/plugins/agent_build_history/AgentBuildHistory/history.js
@@ -1,22 +1,3 @@
-Behaviour.specify(".abh-list__button button", "abh-list__button", 0, function(button) {
-  button.onclick = function() {
-    let tr = button.closest("tr");
-    let table = button.closest("table");
-    let rows = tr.querySelectorAll(".abh-hidden");
-    for (row of rows) {
-      row.classList.toggle("jenkins-hidden");
-    }
-    if (button.dataset.hidden === "true") {
-      button.dataset.hidden = "false";
-      button.setAttribute("tooltip", table.dataset.hideText);
-    } else {
-      button.dataset.hidden = "true";
-      button.setAttribute("tooltip", table.dataset.showText);
-    }
-    Behaviour.applySubtree(tr);
-  }
-});
-
 function setCookie(name, value) {
   const expires = new Date();
   expires.setFullYear(expires.getFullYear() + 1); // Cookie expires in 1 year
